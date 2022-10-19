b10625e854d8616273d06fa765b9c9c2683f77b4
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-10-19 14:13:21 +0530

    Changed n

--- factorial.java
+++ factorial.java
@@ -1,7 +1,7 @@
 class factorial{  
     public static void main(String args[]){  
      int i,fact=1;  
-     int number=5;
+     int number=6;
      for(i=1;i<=number;i++){    
          fact=fact*i;    
     }    
